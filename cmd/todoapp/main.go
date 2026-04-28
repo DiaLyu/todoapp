d6cecc85abb4f3273043849e70619ff4b237c41f
Diana Lyukina <05idianka6282@gmail.com>
2026-04-29 02:52:54 +0400

    add frontend

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -22,6 +22,9 @@ import (
 	users_postgres_repository "github.com/DiaLyu/todoapp/internal/features/users/repository/postgres"
 	users_service "github.com/DiaLyu/todoapp/internal/features/users/service"
 	users_transport_http "github.com/DiaLyu/todoapp/internal/features/users/transport/http"
+	web_fs_repository "github.com/DiaLyu/todoapp/internal/features/web/repository/file_system"
+	web_service "github.com/DiaLyu/todoapp/internal/features/web/service"
+	web_transport_http "github.com/DiaLyu/todoapp/internal/features/web/transport/http"
 	"go.uber.org/zap"
 
 	_ "github.com/DiaLyu/todoapp/docs"
@@ -78,6 +81,11 @@ func main() {
 	statisticsService := statistics_service.NewStatisticsService(statisticsRepository)
 	statisticsTransportHTTP := statistics_transport_http.NewStatisticsHTTPHandler(statisticsService)
 
+	logger.Debug("initializing feature", zap.String("feature", "web"))
+	webRepository := web_fs_repository.NewWebRepository()
+	webService := web_service.NewWebService(webRepository)
+	webTransportHTTP := web_transport_http.NewWebHTTPHandler(webService)
+
 	logger.Debug("initializing HTTP server")
 	httpServer := core_http_server.NewHTTPSServer(
 		core_http_server.NewConfigMust(),
@@ -104,6 +112,7 @@ func main() {
 		apiVersionRouterV1,
 		//apiVersionRouterV2,
 	)
+	httpServer.RegisterRoutes(webTransportHTTP.Routes()...)
 	httpServer.RegisterSwagger()
 
 	if err := httpServer.Run(ctx); err != nil {
